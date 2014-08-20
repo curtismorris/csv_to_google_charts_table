484604f3b73d2b5188e8d52a420b0d76a6dce463
Curtis Morris <curtismorris@gmail.com>
2014-08-20 10:13:48 -0500

    Test to search output file for string
    Added a test to search for a string in the output file generate by CI
    vendors.

--- tests/table_checker.py
+++ tests/table_checker.py
@@ -1,13 +1,13 @@
 #This test is meant to run after the script is run to generate an html table from the included CSV file. The test will look for the word "Walters" which is the last name of one of the people in the test file.
 
+# The script will be run on the CI server / service using the following arguments for the input & output files
+# python csv_to_google_charts_table.py example_csv.csv out.html
+
 import sys
 
-string_to_search = "The man's name is John Walters and he lives on 123 Fake Street"
-string_to_find = "thes"
+string_to_find = "Walters"
 
-search_result = string_to_search.find(string_to_find)
-
-if search_result > 0:
-  print"Found"
+if string_to_find in open("../out.html").read():
+  print "Found"
 else:
   print"Not Found"
